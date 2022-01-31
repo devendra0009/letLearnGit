0ec1dbe54af90720f80314b72c8aedc0f094ee53
Davendra Singh Bedwal <devendrabedwal4@gmail.com>
2022-01-31 20:30:07 +0530

    all in one function created

---
+++ allInOne.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=9; int b=9;
+        cout<<a-b;
+        cout<<"The division of a and b is "<<a/b;
+        cout<<"The multiplication of a and b is "<<a*b;
+        cout<<"Sum of a and b is "<<a+b;
+    return 0;
+}
